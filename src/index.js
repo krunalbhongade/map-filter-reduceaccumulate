54b1950f5d45b558a7674f01f946b5980fed20a0
Krunal <126875304+krunalbhongade@users.noreply.github.com>
2023-09-14 06:07:44 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// import emojipedia from "./emojipedia";
+
+// const newEmojipedia = emojipedia.map(function (emojiEntry) {
+//   return emojiEntry.meaning.substring(0, 100);
+// });
+
+// console.log(newEmojipedia);
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map((x) => x * 2);
+
+// console.log(newNumbers);
+
+//Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter((num) => num > 10);
+// console.log(newNumbers);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+const newNumber = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
+console.log(newNumber);
+//Find - find the first item that matches from an array.
+// const newNumber = numbers.find((num) => num > 10);
+
+// console.log(newNumber);
+//FindIndex - find the index of the first item that matches.
+// const newNumber = numbers.findIndex((num) => num > 10);
+// console.log(newNumber);
